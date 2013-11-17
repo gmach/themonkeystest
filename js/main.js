ea2a8770020dfd3182e96c917e658c9a136f4e2e
Gavin Mach <gavinmach@gmail.com>
2013-11-17 21:17:53 +1100

    first release before monkeytestjs.io

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+function validateMyForm() { 
+	var enqType = document.getElementById("enqType");
+	if (enqType.value == "Product complaint") {
+		return validateFormField("productName") && validateFormField("productSize") && validateFormField("usebyDate") && validateFormField("batchCode");
+	}
+}
 
+function validateFormField(fieldId) {
+	var name = document.getElementById(fieldId).name;
+	var value = document.getElementById(fieldId).value;
+	if (value == null || value == "") {
+		alert ("Please enter a value for " + name);
+		return false;
+	} else {
+		return true;
+	}
+}
